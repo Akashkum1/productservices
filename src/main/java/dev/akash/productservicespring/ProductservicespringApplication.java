557cd52333634d0a29d7ff449fe343c753cba1e7
akashneos <akash.kumavat@neosoftmail.com>
2023-09-28 22:45:36 +0530

    Initilized the spring application

---
+++ src/main/java/dev/akash/productservicespring/ProductservicespringApplication.java
@@ -0,0 +1,13 @@
+package dev.akash.productservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductservicespringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductservicespringApplication.class, args);
+    }
+
+}
